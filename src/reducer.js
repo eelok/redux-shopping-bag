74be74b9160b31181a39767c01f3278f4540e955
eelok <eelok@mail.ru>
2020-11-19 20:00:00 +0100

    redux shopping bag project

---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+import {DECREASE, INCREASE} from "./actions";
+
+
+//как использовать если бы сделал так
+// const reducer = (state, action) =>
+function reducer(state, action){
+    return state;
+}
+
+export default reducer;
