596092efd0f709c51b8799428c2788e03c72ca61
eelok <eelok@mail.ru>
2020-11-21 16:22:34 +0100

    make code readable

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -3,51 +3,54 @@ import CartItem from "./CartItem";
 import {connect} from "react-redux";
 import {CLEAR_CART, GET_TOTALS} from "../actions";
 
-const CartContainer = ({cart = [], total, dispatch }) => {
+const CartContainer = ({cart = [], total, dispatch}) => {
 
     useEffect(() => {
         dispatch({type: GET_TOTALS});
-    })
-  if (cart.length === 0) {
+    });
+
+    if (cart.length === 0) {
+        return (
+            <section className="cart">
+                {/* cart header */}
+                <header>
+                    <h2>your bag</h2>
+                    <h4 className="empty-cart">is currently empty</h4>
+                </header>
+            </section>
+        );
+    }
     return (
-      <section className="cart">
-        {/* cart header */}
-        <header>
-          <h2>your bag</h2>
-          <h4 className="empty-cart">is currently empty</h4>
-        </header>
-      </section>
+        <section className="cart">
+            {/* cart header */}
+            <header>
+                <h2>your bag</h2>
+            </header>
+            {/* cart items */}
+            <article>
+                {cart.map(item => {
+                    return <CartItem key={item.id} {...item} />;
+                })}
+            </article>
+            {/* cart footer */}
+            <footer>
+                <hr/>
+                <div className="cart-total">
+                    <h4>
+                        total <span>${total}</span>
+                    </h4>
+                </div>
+                <button className="btn clear-btn"
+                        onClick={() => dispatch({type: CLEAR_CART})}
+                >clear cart
+                </button>
+            </footer>
+        </section>
     );
-  }
-  return (
-    <section className="cart">
-      {/* cart header */}
-      <header>
-        <h2>your bag</h2>
-      </header>
-      {/* cart items */}
-      <article>
-        {cart.map(item => {
-          return <CartItem key={item.id} {...item} />;
-        })}
-      </article>
-      {/* cart footer */}
-      <footer>
-        <hr />
-        <div className="cart-total">
-          <h4>
-            total <span>${total}</span>
-          </h4>
-        </div>
-        <button className="btn clear-btn"
-            onClick={() => dispatch({type: CLEAR_CART})}
-        >clear cart</button>
-      </footer>
-    </section>
-  );
 };
 
-function mapStateToProps(store){
-    return { cart: store.cart, total: store.total }
+function mapStateToProps(store) {
+    return {cart: store.cart, total: store.total}
 }
+
 export default connect(mapStateToProps)(CartContainer);
--- src/reducer.js
+++ src/reducer.js
@@ -34,15 +34,15 @@ function reducer(state, action) {
         };
     }
     if (action.type === GET_TOTALS) {
-        let { total, amount } = state.cart.reduce(
+        let {total, amount} = state.cart.reduce(
             (cartTotal, cartItem) => {
-                const{price, amount} =cartItem;
+                const {price, amount} = cartItem;
                 const itemTotal = price * amount;
                 cartTotal.total += itemTotal;
                 cartTotal.amount += amount;
                 return cartTotal;
             },
-             {
+            {
                 total: 0, amount: 0
             });
         total = parseFloat(total.toFixed(2));
